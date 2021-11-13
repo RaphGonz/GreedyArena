cee378eeaa6ad59b5aa9bfb12b4126168024ad6f
Jasmin <jasmin.pradat@telecom-sudparis.eu>
2021-11-13 12:59:37 +0100

    player controller et input manager, le player bouge et saute, et son bras s'oriente selon la direction de la souris

--- Assets/Scripts/Jasmin/InputManager.cs
+++ Assets/Scripts/Jasmin/InputManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    [SerializeField] PlayerController playerController;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +14,31 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateInput();
     }
 
     void UpdateInput()
     {
+        float xValue = Input.GetAxis("Horizontal");
+
+        if (Mathf.Abs(xValue) > 0.1f)
+        {
+            xValue = Mathf.Sign(xValue);
+            playerController.MoveX(xValue);
+        }
+
+
+        if (Input.GetButtonDown("Jump")) {
+            playerController.Jump();
+        }
+
+        playerController.RotatePivotPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        //Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+        if (Input.GetMouseButton(0))
+        {
+            playerController.Shoot();
+        }
     }
+
 }
