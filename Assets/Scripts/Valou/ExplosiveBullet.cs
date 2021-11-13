c34c59b783248fbeac6636af1c823df32aa4b055
ValouBambou <theval358@gmail.com>
2021-11-13 19:09:57 +0100

    add explosive bullets

---
+++ Assets/Scripts/Valou/ExplosiveBullet.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Guillaume;
+using UnityEngine;
+
+public class ExplosiveBullet : Bullet
+{
+    public float ExplosionRadius;
+    protected override void OnCollisionEnter2D(Collision2D collision)
+    {
+        Collider2D[] result = new Collider2D[10];
+        Physics2D.OverlapCircle(collision.GetContact(0).point, ExplosionRadius, new ContactFilter2D(), result);
+        foreach (var col in result)
+        {
+            if (col.transform.tag.Equals(TargetTag))
+            {
+                col.gameObject.GetComponent<Entity>().TakeDamage(Damage);
+            }
+        }
+        Destroy(gameObject);
+    }
+}
