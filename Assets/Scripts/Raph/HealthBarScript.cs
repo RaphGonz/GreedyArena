0e0032ac49c82d50fbcea20dff3397346743224b
RaphGonz <62506989+RaphGonz@users.noreply.github.com>
2021-11-14 10:52:55 +0100

    health bar flash

--- Assets/Scripts/Raph/HealthBarScript.cs
+++ Assets/Scripts/Raph/HealthBarScript.cs
@@ -7,6 +7,8 @@ public class HealthBarScript : MonoBehaviour
 {
     public Slider slider;
 
+    public Image damageShade;
+
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
@@ -15,5 +17,20 @@ public class HealthBarScript : MonoBehaviour
     public void SetHealth(int health)
     {
         slider.value = health;
+        if(slider.value == 1)
+        {
+            StartCoroutine(DamageShade());
+        }
+    }
+
+    IEnumerator DamageShade()
+    {
+        
+        while(true){
+            float t = Time.time;
+            byte alpha = (byte)( 50 * (0.5f + 0.5f * Mathf.Sin(10*t)) );
+            damageShade.color = new Color32(150,0,0,alpha); //Une fonction qui varie entre 0 et 1
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
