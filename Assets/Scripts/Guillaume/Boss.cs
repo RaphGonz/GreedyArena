46e0b40d163ee0afce1d526e9b013ae538f330d2
Àre <arematurana@hotmail.com>
2021-11-14 13:42:27 +0100

    invulnerabilityFrames

--- Assets/Scripts/Guillaume/Boss.cs
+++ Assets/Scripts/Guillaume/Boss.cs
@@ -13,6 +13,9 @@ public class Boss : Entity
     private float timeStart;
     private int currentPosition = 0;
 
+    public bool invulnerable = false;
+    public SpriteRenderer bossColor;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -35,11 +38,36 @@ public class Boss : Entity
 
     public override void TakeDamage(int damage)
     {
-        _health -= damage;
-        if (_health <= 0)
+        if (!invulnerable)
         {
-            Debug.Log("Partie Terminée !");
-            Destroy(gameObject);
+            StartCoroutine(inVulnerabilityFrames());
+            _health -= damage;
+
+            if (_health <= 0)
+            {
+                Debug.Log("Partie Terminée !");
+                Destroy(gameObject);
+            }
         }
     }
+
+    IEnumerator inVulnerabilityFrames()
+    {
+        invulnerable = true;
+        var _initialColor = bossColor.color;
+        var _color = new Color(bossColor.color.r, bossColor.color.g, bossColor.color.b, 0.2f);
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (i % 2 == 0)
+                bossColor.color = _color;
+            else
+                bossColor.color = _initialColor;
+            yield return new WaitForSeconds(0.1f);
+
+        }
+        invulnerable = false;
+    }
+
+
 }
